51b7345710390dfc513e14e927613a7d46f5ab45
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-12-16 17:36:59 +0900

    1.2 Static Pre Rendering

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
+import { useState } from "react";
+
 export default function Home() {
+  const [counter, setCounter] = useState(0);
   return (
     <div>
-      <h1>Hello</h1>
+      <h1>Hello {counter}</h1>
+      <button onClick={() => setCounter((prev) => prev + 1)}>+</button>
     </div>
   );
 }
