1fed48524b068d7cf70e0401007e622b16cc9308
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2021-12-17 18:46:57 +0900

    2.4 Recap

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home({ results }) {
       <Seo title="Home" />
       {results?.map((movie) => (
         <div className="movie" key={movie.id}>
-          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
         </div>
       ))}
@@ -17,6 +17,9 @@ export default function Home({ results }) {
           grid-template-columns: 1fr 1fr;
           padding: 20px;
           gap: 20px;
+        }
+        .movie {
+          cursor: pointer;
         }
         .movie img {
           max-width: 100%;
